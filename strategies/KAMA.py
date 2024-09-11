3888921708ebce50375e163052edb60434839693
Devan Veach <dazetrading0880@gmail.com>
2024-09-11 09:39:59 -0500

    deleted KAMA.py

--- strategies/KAMA.py
+++
@@ -1,8 +0,0 @@
-from strategy import Strategy
-
-class KAMA(Strategy):
-    def __init__(self, df, **kwargs):
-        super.__init__(df=df **kwargs==kwargs)
-    
-    def custom_indicator(self, ):
-        pass
