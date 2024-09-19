3b175da788992650d97fb1f3b71c3a254f546edf
Devan Veach <dazetrading0880@gmail.com>
2024-09-19 00:13:59 -0500

    refactored process_message function

--- dataframe_manager.py
+++ dataframe_manager.py
@@ -11,10 +11,10 @@ class DF_Manager():
 
 
     def _to_df(self, dict:dict):
-        key = next(iter(dict))
-        df = pd.DataFrame(dict[key]) # converts to df
+        key = next(iter(dict)) # gets the first key to the dictionary
 
-        if key == 'candles':
+        if key == 'candles': #not heartbeats only candles
+            df = pd.DataFrame(dict[key]) # converts to df
             df['date'] = pd.to_datetime(df['start'].astype(float), unit='s')
             try:
                 """this is for WSclient"""
@@ -24,23 +24,17 @@ class DF_Manager():
                 df = df.drop(column=['start'])
             df.loc[:, df.columns != 'date'] = df.loc[:, df.columns != 'date'].apply(pd.to_numeric, errors='coerce')
 
-            df = df.ffill()
+            #df = df.ffill()
+            df = df.infer_objects(copy=False)#forward fill NANs
         return df
 
     def process_message(self, message):
-        msg_data = json.loads(message)
-        candles_data = msg_data.get('events', [])[0].get('candles', [])
+        msg_data = json.loads(message) # loads data into json format
+        candles_data = msg_data.get('events', [])[0].get('candles', []) # retrives data if there is some if not an empty list
 
-        if candles_data:
-            candles_data = {'candles': candles_data}
-            if self.first_candle:
-                self.df = self._to_df(candles_data)
-                self.first_candle = False
-
-                print(self.df.tail())
-            else:
-                self.new_df = self._to_df(candles_data)
-                self.df = pd.concat([self.df, self.new_df], ignore_index=True)
-
-                print(self.df.tail())
+        if candles_data: # if not an empty list
+            candles_data = {'candles': candles_data} #rename to work with _to_df
+            self.new_df = self._to_df(candles_data)
+            self.df = pd.concat([self.df, self.new_df], ignore_index = True)
+            print(self.df.tail())
 
