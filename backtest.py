6a44dc8f5c7e70c342068a7ac594b5a74dbd55a5
tomas maxim <svkmongolec@gmail.com>
2024-11-07 21:14:23 +0100

    adding a function in datave_interaction - get_hyper_from_db, currenlty getting only name of symbol

--- backtest.py
+++ backtest.py
@@ -94,48 +94,53 @@ def run_basic_backtest():
         # fig.show()
 
         # print(pf.stats())
-run_basic_backtest()
+#run_basic_backtest()
 
 
 
 
 def run_hyper():
+    print("running hyper")
     dict_df = database_interaction.get_historical_from_db(granularity=granularity,
-                                                          symbols=product,
-                                                          num_days=300)
+                                                          symbols=symbols,
+                                                          num_days=100)
 
     #dict_df = utils.heikin_ashi_transform(dict_df)
     
+    for key, value in dict_df.items():
+        current_df = {key:value}
+
     
-    strat = RSI_ADX(dict_df)
-    #strat.custom_indicator()
+        strat = RSI_ADX(current_df)
+        #strat.custom_indicator()
 
-    hyper = Hyper(strategy_object=strat,
-                  close=strat.close,
-                  rsi_window=np.arange(10, 30, step=5),
-                  buy_threshold=np.arange(5, 51, step=5),
-                  sell_threshold = np.arange(50, 96, step=5),
-                  adx_buy_threshold = np.arange(20, 81, step=10),
-                  adx_time_period=np.arange(10, 30, step=5))
-    #print(hyper.returns.to_string())
-    #print(type(hyper.returns))
+        hyper = Hyper(strategy_object=strat,
+                    close=strat.close,
+                    rsi_window=np.arange(10, 30, step=5),
+                    buy_threshold=np.arange(5, 51, step=5),
+                    sell_threshold = np.arange(50, 96, step=5),
+                    adx_buy_threshold = np.arange(20, 81, step=10),
+                    adx_time_period=np.arange(10, 30, step=5))
+        #print(hyper.returns.to_string())
+        #print(type(hyper.returns))
 
-    # fig = hyper.returns.vbt.volume(# this line is now volume for a 3D
-    #     x_level = 'cust_fast_window',
-    #     y_level = 'cust_slow_window',
-    #     z_level = 'cust_efrato_window',
-    # )
+        # fig = hyper.returns.vbt.volume(# this line is now volume for a 3D
+        #     x_level = 'cust_fast_window',
+        #     y_level = 'cust_slow_window',
+        #     z_level = 'cust_efrato_window',
+        # )
 
-    # fig = hyper.returns.vbt.heatmap(
-    # x_level = 'cust_fast_window',
-    # y_level = 'cust_slow_window')
-    #fig.show()
-    #database_interaction.export_hyper_to_db(hyper.returns, strat)
+        # fig = hyper.returns.vbt.heatmap(
+        # x_level = 'cust_fast_window',
+        # y_level = 'cust_slow_window')
+        #fig.show()
+        database_interaction.get_hyper_from_db(strat)
+        #database_interaction.export_hyper_to_db(hyper.returns, strat)
 
-    print(f"The maximum return was {hyper.returns.max()}")
-    params = ['rsi_window','buy_threshold','sell_threshold','adx_buy_threshold','adx_time_period']
-    for i,value in enumerate(params):
-        print(f'{value}: {hyper.returns.idxmax()[i]}')
+        # print(f"The maximum return was {hyper.returns.max()}")
+        # params = ['rsi_window','buy_threshold','sell_threshold','adx_buy_threshold','adx_time_period']
+        # for i,value in enumerate(params):
+        #     print(f'{value}: {hyper.returns.idxmax()[i]}')
 
-#run_hyper()
+run_hyper()
 
