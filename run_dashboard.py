dbf27c3afd8dd53895ec7e7c7f33842c928f9eb2
tomas maxim <svkmongolec@gmail.com>
2025-01-07 20:28:42 +0100

    deleting run dashboard since we gonna use website for simplicity

--- run_dashboard.py
+++
@@ -1,7 +0,0 @@
-from optuna_dashboard import app
-
-# Set the storage URL for the dashboard
-app.storage_url = "sqlite:///E:/database/new_backtest.db"
-
-# Export the WSGI application
-application = app
